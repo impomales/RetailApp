567cafff4669b48368d7d11697048b070e32f409
Isaias <impomales@gmail.com>
2017-02-20 19:26:09 +0000

    added testing for connecting to server

--- test.js
+++ test.js
@@ -4,9 +4,10 @@ var categorySchema = require('./schemas/category');
 var productSchema = require('./schemas/product');
 var userSchema = require('./schemas/user');
 var superagent = require('superagent');
+var app = require('./server');
 
 /* will add more tests later */
-
+// schema tests.
 describe('Mongoose Schemas', function() {
     var Category = mongoose.model('Category', categorySchema, 'categories');
     var Product = mongoose.model('Product', productSchema, 'products');
@@ -28,6 +29,28 @@ describe('Mongoose Schemas', function() {
         });        
         it('has a "displayPrice" virtual', function() {
             assert.equal(product.displayPrice, '$999.99');
+        });
+    });
+});
+
+//server tests.
+describe('server', function() {
+    var server;
+    
+    beforeEach(function() {
+        server = app().listen(process.env.PORT);
+    });
+    
+    afterEach(function() {
+        server.close();
+    });
+    
+    it('prints out "hello...it\'s me" when user goes to /', function(done) {
+        superagent.get('http://' + process.env.IP + ':' + process.env.PORT + '/', function(err, res) {
+            assert.ifError(err);
+            assert.equal(res.status.OK);
+            assert.equal(res.text, "hello...it's me");
+            done();
         });
     });
 });
