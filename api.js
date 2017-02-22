42e21c85d1821e2b5b769455f2fe461916988876
Isaias <impomales@gmail.com>
2017-02-22 18:18:47 +0000

    add prduct model

--- api.js
+++ api.js
@@ -4,6 +4,13 @@ var status = require('http-status');
 module.exports = function(wagner) {
     var api = express.Router();
     
+    /*api.get('/product/id/:id', wagner.invoke(function(Product) {
+        return function(req, res) {
+            Product.findOne({ _id: req.params.id }, 
+            handleOne.bind(null, 'product', res));
+        }
+    }));*/
+    
     api.get('/category/id/:id', wagner.invoke(function(Category) {
         return function(req, res) {
             Category.findOne({ _id: req.params.id }, function(err, category) {
@@ -40,3 +47,20 @@ module.exports = function(wagner) {
     
     return api;
 };
+
+function handleOne(property, res, error, result) {
+    if (error) {
+        return res.
+                status(status.INTERNAL_SERVER_ERROR).
+                json({ error: error.toString() });
+    }
+    if (!result)  {
+        return res.
+                status(status.NOT_FOUND).
+                json({ error: 'not found' });
+    }
+    
+    var json = {};
+    json[property] = result;
+    res.json(json);
+}
