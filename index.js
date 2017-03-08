936187468d00ef92e49eaf673b88e26f4e1857df
Isaias <impomales@gmail.com>
2017-03-08 21:36:14 +0000

    add fb auth

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var wagner = require('wagner-core');
 require('./models')(wagner);
 
 var app = express();
+require('dotenv').load();
 
 app.get('/', function(req, res) {
     res.send('hello...');
 });
+
+wagner.invoke(require('./auth'), {app: app});
 app.use('/api/v1', require('./api')(wagner));
 
 app.listen(process.env.PORT);
