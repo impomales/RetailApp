6a246f1565e7ab753f428ad549d8ff512ac467d0
Isaias <impomales@gmail.com>
2017-03-15 18:00:48 +0000

    add a log in page

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ app.get('/', function(req, res) {
 wagner.invoke(require('./auth'), {app: app});
 app.use('/api/v1', require('./api')(wagner));
 
+app.use(express.static('./static', {maxAge: 4 * 60 * 60 * 1000 /* 2 hrs */}));
+
 app.listen(process.env.PORT);
 console.log("listening on port " + process.env.PORT);
